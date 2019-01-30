0d12d277758b4a588aeb3407006ec3f3d840ae0c
Olivier Perriot <olivier.perriot@free.fr>
2019-01-30 22:46:44 +0000

    Add run control

--- liftree/templates/wapi/run/run.js
+++ liftree/templates/wapi/run/run.js
@@ -1,6 +1,31 @@
 
 const run_status = '{{ data.status }}'
 const ansi_up = new AnsiUp;
+const $run_control_start = $('#run_control_start')
+const $run_control_watch = $('#run_control_watch')
+const $run_control_kill = $('#run_control_kill')
+const $run_control_end = $('#run_control_end')
+const $output = $('#output')
+const $watch = $('#run_control_watch .eye.icon')
+
+// not yet implemented
+$run_control_kill.addClass('disabled')
+
+$run_control_start.click( () => {
+  console.log("go start")
+  $output.scrollTop(0);  
+});
+$run_control_end.click( () => {
+  console.log("go end")
+  $output.scrollTop($output[0].scrollHeight);
+});
+
+$run_control_watch.click( () => {
+  $watch.toggleClass('slash')
+  if (!$watch.hasClass('slash')) {
+    get_run_output()
+  }
+});
 
 const update_page = (response) => {
     let status = response.results.status.status
@@ -8,13 +33,17 @@ const update_page = (response) => {
     if ( run_status == 'finished' ) {
       let html = ansi_up.ansi_to_html(response.results.output);
       document.getElementById('output').innerHTML = html
+      $run_control_watch.addClass('disabled')
+      $run_control_kill.addClass('disabled')
     } else {
       if (status == 'finished') {
         location.reload();
       } else {
         let html = ansi_up.ansi_to_html(response.results.output);
         document.getElementById('output').innerHTML = html
-        setTimeout(get_run_output, 1000);
+        if (!$watch.hasClass('slash')) {
+          setTimeout(get_run_output, 1000);
+        }
       }
     }
 }
