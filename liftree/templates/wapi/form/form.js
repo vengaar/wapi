9fe40372047d00abaed807008d281787e49cd305
Olivier Perriot <olivier.perriot@free.fr>
2019-02-07 23:19:33 +0000

    Add configurations and rework javascript form

--- liftree/templates/wapi/form/form.js
+++ liftree/templates/wapi/form/form.js
@@ -1,6 +1,19 @@
-$.fn.api.settings.api = {
-  'playbook_tasks': '/ansible-ws/tasks?playbook={playbook}',
-};
+// const urlParams = new URLSearchParams(window.location.search);
+// console.log(urlParams.get("aa"))
+
+// @todo email not updated
+
+
+// util function to extract
+const sui_list_to_selected_options = (values) => {
+  let options = []
+  if (values !== undefined && values.length > 0) {
+    options = values.split(',').map(x => new Object({'name': x, 'value': x, 'selected': true}));
+  }
+  return options
+}
+
+
 
 let cmdline_base = '{{ extra.wapi_config.ansible_cmdline.playbook }}'
 let cmdline_options = $('#options').val()
@@ -9,8 +22,45 @@ let cmdline_tags_apply = ''
 let cmdline_tags_skip = ''
 let cmdline_tasks = ''
 let extra_vars = JSON.parse('{{ wapi.extra_vars|wapi_defaults_extra_vars|to_json }}')
-//console.log(extra_vars)
+// console.log('extra_vars', extra_vars)
 //console.log(cmdline_options)
+
+
+
+const wapi = {{ wapi|to_json }}
+// console.log(wapi.extra_vars)
+const form_fields_check = {}
+
+
+
+const init_extra_var = extra_var => {
+//   console.log(extra_var.name, extra_var)
+  let name = extra_var.name
+  if ('check' in extra_var) {
+    form_fields_check[name] = extra_var.check
+  } else if ('attributes' in extra_var && extra_var.attributes.indexOf('required') > -1) {
+    form_fields_check[name] = 'empty'
+  } 
+
+  if ('search' in extra_var) {
+    $('#extra_vars-' + extra_var.name).dropdown({
+      apiSettings: {
+        url: extra_var.search + '?' + extra_var.search_params,
+        cache: true
+      },
+      onChange: update_extra_var_dropdown,
+      clearable: true,
+      values: sui_list_to_selected_options(extra_var.default),
+      filterRemoteData: true,  
+    });
+  }
+
+}
+
+wapi.extra_vars.forEach( (elem) => {
+  init_extra_var(elem)
+})
+console.log('form_fields_check', form_fields_check)
 
 
 
@@ -88,6 +138,8 @@ $('.ui.toggle.checkbox').checkbox({
 
 function update_extra_var_dropdown(value, text, $selectedItem) {
   let name = this.id.replace('extra_vars-', '')
+//   console.log(this.id, value)
+  delete extra_vars[name]
   if (value === '') {
     delete extra_vars[name]
   }
@@ -105,29 +157,7 @@ $('.extra_vars.ui.dropdown.choices').dropdown({
   clearable: true,
 })
 
-// util function to extract
-const sui_list_to_selected_options = (values) => {
-  let options = []
-  if (values.length > 0) {
-    options = values.split(',').map(x => new Object({'name': x, 'value': x, 'selected': true}));
-  }
-  return options
-}
 
-{% for param in wapi.extra_vars %}{% if param.search is defined %}
-
-$('#extra_vars-{{ param.name }}').dropdown({
-  apiSettings: {
-    url: '{{ param.search }}?{{ param.search_params }}',
-    cache: true
-  },
-  onChange: update_extra_var_dropdown,
-  clearable: true,
-  values: sui_list_to_selected_options('{{ param.default }}'),
-  filterRemoteData: true,  
-});
-
-{% endif %}{% endfor %}
 
 const display_cmdline = () => {
   let cmdline = [
@@ -156,58 +186,74 @@ const display_cmdline = () => {
 }
 display_cmdline()
 
-
-
-// $('#launch_playbook')
-$('#playbook_form')
-.api({
+$('#playbook_form').form({
+  fields: form_fields_check
+}).api({
     contentType: 'application/json',
     dataType: 'json',
     url: '/ansible-ws/launch',
     method:'POST',
     serializeForm: true,
-    beforeSend: function(settings) {
-      //console.log("Data submitted:",settings);
-      return $('#playbook_form').form('is valid');
-    },
     onSuccess: function(response, element, xhr) {
-        //console.log('success');
-        //console.log(response);
+//         console.log('success');
+//         console.log(response);
         let url = '/show?path={{ extra.wapi_config.runs_dir }}/' + response.results.runid + '/run.status'
-        // window.location.replace(url)
+//         window.location.replace(url)
         window.open(url)
-        $(this).removeClass('loading')
-        return false
     },
     onError: function(errorMessage, element, xhr) {
         show_error(errorMessage)
-        return false
     },
     onFailure: function(response, element) {
         show_error(response)
-        return false
     }
 })
-.form({
-    onSuccess: function (event) {
-      $(this).addClass('loading')
-      event.preventDefault();
-      //console.log('valid');
-    },
-    onFailure: function (event) {
-      //console.log('NOT valid');
-      return false;
-    },    
 
-  fields: {
-{% for param in wapi.extra_vars %}
-{% if param.check is defined %}
-    {{ param.name }} : '{{ param.check }}',
-{% elif 'required' in param.attributes|default([]) %}
-    {{ param.name }} : 'empty',
-{% endif %}
-{% endfor %}
+
+
+if ('configurations' in wapi) {
+//   console.log(wapi.configurations)
+  const load_configuration = (configuration) => {
+    console.log('load_configuration', configuration)
+//     console.log('load_configuration', wapi.configurations[configuration])
+    for (let key in wapi.configurations[configuration]) {
+//       console.log(key)
+      const value = wapi.configurations[configuration][key]
+//       console.log(value)
+      let $extra_var = $('#extra_vars-' + key)
+      if ($extra_var.hasClass('choices')) {
+//         console.log('update choice', key, 'with', value)
+        $extra_var.dropdown('set exactly', value)
+      } else if ($extra_var.hasClass('dropdown')) {
+//         console.log('update dropdown', key, 'with', value)
+        const options = (typeof value === 'string') ? [value] : value;
+        const values = options.map(x => new Object({'name': x, 'value': x}))
+//         console.log(values)
+        $extra_var.dropdown('change values', values)
+        $extra_var.dropdown('set exactly', value)
+      } else if ($extra_var.hasClass('checkbox')) {
+//         console.log('update checkbox', key, 'with', value) 
+        if (value) {
+          $extra_var.checkbox('check')
+        } else {
+          $extra_var.checkbox('uncheck')
+        }
+      } else if ($extra_var.hasClass('input')) {
+//         console.log('update input', key, 'with', value)
+        $extra_var.val(value)
+      }
+    }
+    // check form validation
+    $('#playbook_form').form('validate form')
   }
-})
+  const configurations_values = Object.keys(wapi.configurations).map(x => new Object({'name': x, 'value': x}))
+//   console.log('configurations_values', configurations_values)
+  $('#wapi-configuration').dropdown({
+    clearable: true,
+    onChange: load_configuration,
+    values: configurations_values,
+  })
+}
+
 
 console.log('ok form')
