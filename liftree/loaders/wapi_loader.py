f831240052c855e00e102915e9b7b2b2bd650128
Olivier Perriot <olivier.perriot@free.fr>
2019-02-09 18:02:38 +0000

    Add try/except and error/warning on data returned

--- liftree/loaders/wapi_loader.py
+++ liftree/loaders/wapi_loader.py
@@ -1,10 +1,13 @@
 import yaml
 
 def get_data(path, params):
-    with open(path, 'r') as stream:
-        data = yaml.load(stream)
-    if 'vars' in data[0]:
-        wapi = data[0]['vars'].get('wapi', dict())
-    else:
-        wapi = dict()
+    try:
+        with open(path, 'r') as stream:
+            data = yaml.load(stream)
+        if 'vars' in data[0]:
+            wapi = data[0]['vars'].get('wapi', dict())
+        else:
+            wapi = dict(warning='No wapi data found')
+    except Exception as e:
+        wapi = dict(error=f'{str(e)}')
     return wapi
