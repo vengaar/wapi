9887512a8e432dd4422afa6f0f5b516bbe1ec297
Olivier Perriot <olivier.perriot@free.fr>
2019-02-13 23:35:41 +0000

    Diplay paths relative to role

--- liftree/loaders/wapi_role.py
+++ liftree/loaders/wapi_role.py
@@ -1,16 +1,17 @@
 import yaml
 import os
+from pathlib import PurePosixPath
 
 def get_data(path, params):
     role_dir = os.path.dirname(os.path.dirname(path))
     role_files = []
     for root, subFolder, files in os.walk(role_dir):
-#         for directory in directories:
-#             print os.path.join(root, directory) 
         dirname = os.path.basename(root)
         for filename in files: 
+            value = os.path.join(root,filename)
+            name = PurePosixPath(value).relative_to(role_dir)
             role_files.append({
-                'name': f'{dirname} > {filename}',
-                'value': os.path.join(root,filename)
+                'name': name,
+                'value': value
             })
     return role_files
