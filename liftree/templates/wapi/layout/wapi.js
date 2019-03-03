8fa1c0c1db06373b191f5bfa6032f860f4d61924
Olivier Perriot <olivier.perriot@free.fr>
2019-03-03 12:38:06 +0000

    Add star info for host_vars and run, and fix wrong star display

--- liftree/templates/wapi/layout/wapi.js
+++ liftree/templates/wapi/layout/wapi.js
@@ -18,34 +18,38 @@ $('.ui.search').search({
  * stars
  */
 let wapi_stars = localStorage.getItem('wapi-stars')
-console.log(wapi_stars)
+// console.log(wapi_stars)
 wapi_stars = wapi_stars === null ? {} : JSON.parse(wapi_stars) 
-console.log(wapi_stars)
+// console.log(wapi_stars)
 if (path in wapi_stars) {
 	$('.wapi-start').addClass('yellow')
 	$('.wapi-start').removeClass('outline')
 }
 const stars_udapte = () => {
+	// console.log('update star for', path)
 	const $starts = $('.wapi-start')
 	$starts.toggleClass('yellow')
 	$starts.toggleClass('outline')
 	if ( $starts.hasClass('yellow') ) {
-		wapi_stars[path] = true
+		wapi_stars[path] = {'cat': wapi_star_cat, 'name': wapi_star_name, 'icon': wapi_star_icon}
 	} else {
 		delete wapi_stars[path]
-		//wapi_stars = wapi_stars.filter(item => item !== path)
+		// wapi_stars = wapi_stars.filter(item => item !== path)
 	}
-	console.log(wapi_stars)
+	// console.log(wapi_stars)
 	localStorage.setItem('wapi-stars', JSON.stringify(wapi_stars));
 	stars_draw()
 }
 const stars_draw = () => {
-	// console.log(wapi_stars.length)
+	// console.log('stars_draw')
 	const $wapi_stars = $('#wapi-stars')
 	$wapi_stars.empty()
 	for (let star_key in wapi_stars) {
-		console.log(star_key)
-		$wapi_stars.append(`<a class="item" href="/show?path=${star_key}"><i class="${wapi_star_icon} icon"></i>[${wapi_star_cat}] ${wapi_star_name}</a>`)
+		// console.log(star_key)
+		const star_data = wapi_stars[star_key]
+		// console.log(star_data)
+		// [${star_data.cat}]
+		$wapi_stars.append(`<a class="item" href="/show?path=${star_key}"><i class="${star_data.icon} icon"></i>${star_data.name}</a>`)
 	}
 }
 stars_draw()
@@ -56,7 +60,7 @@ $('.wapi-start').click(stars_udapte)
  * Menu
  */
 $('.menu .browse').popup({
-	//inline: true,
+	// inline: true,
 	position: 'bottom left',
 	lastResort: 'bottom left',
 	offset: 10,
