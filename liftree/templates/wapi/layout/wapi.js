abedf13fa224686ce794affb65b69ba10730cae3
Olivier Perriot <olivier.perriot@free.fr>
2019-03-03 11:24:42 +0000

    Add star feature

--- liftree/templates/wapi/layout/wapi.js
+++ liftree/templates/wapi/layout/wapi.js
@@ -14,6 +14,59 @@ $('.ui.search').search({
 	},
 });
 
+/**
+ * stars
+ */
+let wapi_stars = localStorage.getItem('wapi-stars')
+console.log(wapi_stars)
+wapi_stars = wapi_stars === null ? {} : JSON.parse(wapi_stars) 
+console.log(wapi_stars)
+if (path in wapi_stars) {
+	$('.wapi-start').addClass('yellow')
+	$('.wapi-start').removeClass('outline')
+}
+const stars_udapte = () => {
+	const $starts = $('.wapi-start')
+	$starts.toggleClass('yellow')
+	$starts.toggleClass('outline')
+	if ( $starts.hasClass('yellow') ) {
+		wapi_stars[path] = true
+	} else {
+		delete wapi_stars[path]
+		//wapi_stars = wapi_stars.filter(item => item !== path)
+	}
+	console.log(wapi_stars)
+	localStorage.setItem('wapi-stars', JSON.stringify(wapi_stars));
+	stars_draw()
+}
+const stars_draw = () => {
+	// console.log(wapi_stars.length)
+	const $wapi_stars = $('#wapi-stars')
+	$wapi_stars.empty()
+	for (let star_key in wapi_stars) {
+		console.log(star_key)
+		$wapi_stars.append(`<a class="item" href="/show?path=${star_key}"><i class="${wapi_star_icon} icon"></i>[${wapi_star_cat}] ${wapi_star_name}</a>`)
+	}
+}
+stars_draw()
+$('.wapi-start').click(stars_udapte)
+
+
+/**
+ * Menu
+ */
+$('.menu .browse').popup({
+	//inline: true,
+	position: 'bottom left',
+	lastResort: 'bottom left',
+	offset: 10,
+	on: 'click',
+	// on: 'click',
+	delay: {
+		show: 300,
+		hide: 800
+	}
+});
 
 /**
  * error management
@@ -35,4 +88,4 @@ $('.copy_to_clipboard').click( function() {
 	document.execCommand("copy");
 });
 
-console.log('wapi.js OK')
+console.log('OK - wapi.js')
